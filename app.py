2a283ba9a3e1eeb139e1a02a3a540d934cfd4ea4
Caleb Asiamah <znac536@live.rhul.ac.uk>
2026-08-17 00:25:53 +0100

    Set up Flask application

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import sqlite3
+from flask import Flask
 
+#Creating the flask application object. Every route, template and configuration attaches to this object.
+
+app = Flask(__name__)
 
 
 def get_connection():
+    #opens a connection to the sql database file
     connection = sqlite3.connect('database.db')
+    #enables access to columns by name instead of position row['username']) instead of (row[1])
+    connection.row_factory = sqlite3.Row
     return connection
 
+
 def init_db():
+    #Runs every SQL statement in schema.sql in order
     connection = get_connection()
     with open('schema.sql', 'r') as f:
         connection.executescript(f.read())
@@ -14,23 +23,14 @@ def init_db():
     connection.close()
 
 
+#Make sure the database and tables exist before the app starts dealing with requests
 init_db()
 
-connection = get_connection()
-cursor = connection.cursor()
 
-cursor.execute("SELECT * FROM USERS")
-results = cursor.fetchall()
-
-if results:
-    for row in results:
-            print(row)
-else:
-     print("No rows yet.")
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
 
 
-
-connection.close()
